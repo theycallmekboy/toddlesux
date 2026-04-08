81008fe4946d16dbf58b42b7e9b2a77b23dc5065
theycallmekboy <klfn2008@gmail.com>
2026-04-08 23:15:43 +0400

    Update taf-core.user.js

--- taf-core.user.js
+++ taf-core.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         toddlesux
 // @namespace    http://tampermonkey.net/
-// @version      0.0.21111
+// @version      0.0.211111
 // @description  Auto-fills Toddle forms with human-like delays, sub-question support, settings & emergency hide
 // @author       theycallmekboy - made with DS
 // @match        https://web.toddleapp.com/*
